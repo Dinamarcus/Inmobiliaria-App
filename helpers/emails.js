0629ecd9aef4c28fd90e19cf3039727a6f71523a
Dinamarcus <fadimarco@frba.utn.edu.ar>
2024-03-15 11:40:18 -0300

    modified:   helpers/emails.js 	modified:   views/auth/reset-password.pug

--- helpers/emails.js
+++ helpers/emails.js
@@ -61,20 +61,33 @@ const emailOlvide = async (datos) => {
 
   const { email, nombre, token } = datos;
 
-  // Enviar el correo
   await transport.sendMail({
-    from: "BienesRaices.com",
+    from: "noreply@bienesraices.com",
     to: email,
-    subject: "Recuperar tu contraseña en BienesRaices",
-    text: "Reestablece tu contraseña en BienesRaices",
+    subject: "Confirma tu cuenta en BienesRaices",
+    text: "Confirma tu cuenta en BienesRaices",
     html: `
-      <h1>Confirma tu cuenta</h1>
-      <p>Hola ${nombre}, has solicitado reestablecer tu password</p>
-      <p>Presiona el siguiente enlace:</p>
-      <a href="${process.env.BACKEND_URL}:${
-      process.env.PORT ?? 4000
-    }/auth/olvide/${token}">Reestablecer Password</a>
-      <p>Si no realizaste esta petición, ignora este mensaje</p>
+    <body style="background-color: #f0f0f0; padding: 20px;">
+      <div style="max-width: 600px; margin: 0 auto; background-color: #ffffff; border-radius: 10px; box-shadow: 0px 0px 10px rgba(0,0,0,0.1);">
+          <div style="padding: 20px; text-align: center; background-color: #f8f8f8;">
+              <h2 style="margin: 0; color: #333333;">BienesRaices</h2>
+              <p style="margin: 0; color: #777777;">Cambio de Password</p>
+          </div>
+
+          <div style="padding: 20px;">
+              <h1 style="color: #333333;">Hola ${nombre}!</h1>
+              <p style="color: #777777;">Sólo falta un paso para cambiar tu password. Presiona el siguiente enlace:</p>
+              <a href="${
+                process.env.BACKEND_URL
+              }/auth/confirmar/${token}" style="display: inline-block; margin-top: 10px; padding: 10px 20px; background-color: #007bff; color: #ffffff; border-radius: 5px; text-decoration: none;">Confirmar cuenta</a>
+              <p style="color: #777777;">Si no realizaste esta petición, ignora este mensaje</p>
+          </div>
+
+          <div style="padding: 20px; text-align: center; background-color: #f8f8f8;">
+              <p style="margin: 0; color: #777777;">© ${new Date().getFullYear()} BienesRaices. All rights reserved.</p>
+          </div>
+      </div>
+    </body>
     `,
   });
 };
